e08f80bf415d84a82a05af50311fe051a06fc1ed
david.startz <d.startz@userwerk.com>
2019-07-15 22:53:47 +0200

    Listen on every route, output more useful informations like auth, cookies, path, ...

--- api-mock.js
+++ api-mock.js
@@ -9,9 +9,26 @@ app.use(bodyParser.urlencoded({
   extended: false
 })); 
 
-router.all('/test', function (req, res) {
-    console.log('METHOD: ', req.method, '\r\nREQUEST:\r\n', req.body, '\r\n----------------------');
-    res.json({'TYPE': req.method, 'REQUEST': req.body});
+router.all('*', function (req, res) {
+  console.log('URL: ', req.originalUrl || false);
+  console.log('CONTENT-TYPE: ', req.headers['Content-Type'] || false);
+  console.log('USER-AGENT: ', req.headers['user-agent'] || false);
+  console.log('COOKIE: ', req.headers['cookie'] || false);
+  console.log('AUTH: ', req.headers['authorization'] || false);
+  console.log('METHOD: ', req.method);
+  console.log('REQUEST:\r\n', req.body);
+  console.log('----------------------');
+  res.json(
+    {
+      'URL': req.originalUrl || false,
+      'CONTENT-TYPE': req.headers['content-type'],
+      'USER-AGENT': req.headers['user-agent'] || false,
+      'COOKIE': req.headers['cookie'] || false,
+      'AUTH': req.headers['authorization'] || false,
+      'TYPE': req.method,
+      'REQUEST': req.body
+    }
+  );
 });
 
 app.use('/', router)
